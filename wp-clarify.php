0e58ed7e4cdcb2804a0d7a003f55cef5814b074c
Aaron Brazell <aaron@technosailor.com>
2015-03-14 17:48:57 +0000

    lost docs on main file

--- wp-clarify.php
+++ wp-clarify.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP-Clarify
 Plugin URI: http://github.com
-Verion: 1.0-beta4
+Verion: 1.0-beta2
 Author: Aaron Brazell
 Author URI: http://technosailor.com
 Description: Leverages the Clarify API
@@ -10,24 +10,89 @@ License: MIT
 License URI: https://github.com/technosailor/wp-clarify/blob/master/LICENSE
 */
 
-define( 'CLAIRFY_VERSION', '1.0' );
+
+/**
+ * Clarify version of plugin
+ *
+ * @author Aaron Brazell <aaron@technosailor.com>
+ * @since 1.0.0
+ */
+define( 'CLARIFY_VERSION', '1.0' );
+/**
+ * Clarify URL to plugin
+ *
+ * @author Aaron Brazell <aaron@technosailor.com>
+ * @since 1.0.0
+ */
 define( 'CLARIFY_URL',     plugin_dir_url( __FILE__ ) );
+
+/**
+ * Clarify Path to plugin
+ *
+ * @author Aaron Brazell <aaron@technosailor.com>
+ * @since 1.0.0
+ */
 define( 'CLARIFY_PATH',    dirname( __FILE__ ) . '/' );
 
+/**
+ * Base API class
+ */
 require_once( CLARIFY_PATH . '/lib/class.api.php' );
+
+/**
+ * Child class of the base API class. Handles API search functionality
+ */
 require_once( CLARIFY_PATH . '/lib/class.search.php' );
+
+/**
+ * Class to handle all admin UI functionality
+ */
 require_once( CLARIFY_PATH . '/lib/class.admin.php' );
+
+/**
+ * Class to handle API callbacks
+ */
 require_once( CLARIFY_PATH . '/lib/class.webhooks.php' );
+
+/**
+ * Class to handle munging of media embeds
+ */
 require_once( CLARIFY_PATH . '/lib/class.player.php' );
 
+/**
+ * Declares a method to fire on the activation event
+ */
 register_activation_hook( __FILE__, array( 'Clarify', 'activate' ) );
+
+/**
+ * Declares a method to fire on the deactivation event
+ */
 register_deactivation_hook( __FILE__, array( 'Clarify', 'deactivate' ) );
 
 
+/**
+ * Handles the day to day activities of the Clarify Plugin
+ *
+ * @author Aaron Brazell <aaron@technosailor.com>
+ * @since 1.0.0
+ */
 class Clarify {
 
+	/**
+	 * List of media types supported by Clarify
+	 * @var array
+	 *
+	 * @author Aaron Brazell <aaron@technosailor.com>
+	 * @since 1.0.0
+	 */
 	public $supported_media;
 
+	/**
+	 * Constructor
+	 *
+	 * @author Aaron Brazell <aaron@technosailor.com>
+	 * @since 1.0.0
+	 */
 	public function __construct() {
 
 		$this->supported_media = array(
@@ -43,20 +108,28 @@ class Clarify {
 		$this->hooks();
 	}
 
-	public static function activate() {
-		flush_rewrite_rules();
-	}
+	/**
+	 * Activation method
+	 *
+	 * @author Aaron Brazell <aaron@technosailor.com>
+	 * @since 1.0.0
+	 */
+	public static function activate() {}
 
+	/**
+	 * Deactivation method
+	 *
+	 * @author Aaron Brazell <aaron@technosailor.com>
+	 * @since 1.0.0
+	 */
 	public static function deactivate() {}
 
-	public function init() {
-		if( defined( WP_LANG_DIR ) ) {
-			$locale = apply_filters( 'plugin_locale', get_locale(), 'clarify' );
-			load_textdomain( 'clarify', WP_LANG_DIR . '/clarify/clarify-' . $locale . '.mo' );
-			load_plugin_textdomain( 'clarify', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-		}
-	}
-
+	/**
+	 * WordPress hook interactions
+	 *
+	 * @author Aaron Brazell <aaron@technosailor.com>
+	 * @since 1.0.0
+	 */
 	public function hooks() {
 
 		add_action( 'plugins_loaded', function(){
@@ -86,35 +159,66 @@ class Clarify {
 		add_action( 'wp_enqueue_scripts', function() {
 			wp_enqueue_script( 'jquery' );
 		});
-
 	}
 
+	/**
+	 * Instantiates the Player class
+	 *
+	 * @uses Clarify_Players
+	 * @author Aaron Brazell <aaron@technosailor.com>
+	 * @since 1.0.0
+	 */
 	public function do_media() {
 		$players = new Clarify_Players;
 	}
 
+	/**
+	 * Instantiates the Search class
+	 *
+	 * @uses Clarify_Search
+	 * @author Aaron Brazell <aaron@technosailor.com>
+	 * @since 1.0.0
+	 */
 	public function register_search() {
 		new Clarify_Search;
 	}
 
+	/**
+	 * Instantiates the Admin class
+	 *
+	 * @uses Clarify_Admin
+	 * @author Aaron Brazell <aaron@technosailor.com>
+	 * @since 1.0.0
+	 */
 	public function admin() {
 		new Clarify_Admin;
 	}
 
+	/**
+	 * Fires notification to the Clarify API when a post is published
+	 *
+	 * @param $new_status
+	 * @param $old_status
+	 * @param $post
+	 *
+	 * @uses Clarify_Bundle_API
+	 * @author Aaron Brazell <aaron@technosailor.com>
+	 * @since 1.0.0
+	 *
+	 * @return bool
+	 */
 	public function save_post( $new_status, $old_status, $post ) {
 		if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
 			return false;
 
-		$post_id = $post->ID;
-
-		if ( wp_is_post_revision( $post_id ) )
+		if ( wp_is_post_revision( $post->ID ) )
 			return false;
 
 		if( 'publish' != $new_status )
 			return false;
 
 		$api = new Clarify_Bundle_API;
-		return $api->save_bundle( $post_id, $post );
+		return $api->save_bundle( $post->ID, $post );
 	}
 }
 
