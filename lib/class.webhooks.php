f3fd9b84a2ce90c0c142559cb510b3b30805baa3
Keith Casey <keith@caseysoftware.com>
2015-03-29 13:51:42 -0500

    fixing up formatting

--- lib/class.webhooks.php
+++ lib/class.webhooks.php
@@ -9,56 +9,56 @@
  */
 class Clarify_Webhooks_Bundle_Notify {
 
-	/**
-	 * Catches postback data from Clarify and inserts related metadata into the appropriate post ID
-	 *
-	 * @author Aaron Brazell <aaron@technosailor.com>
-	 * @since 1.0.0
-	 * @return bool
-	 */
-	public function recieve() {
-		$data = new stdClass;
-		$data->module_id = false;
-		$data->track_id = false;
-		$data->external_id = false;
+    /**
+     * Catches postback data from Clarify and inserts related metadata into the appropriate post ID
+     *
+     * @author Aaron Brazell <aaron@technosailor.com>
+     * @since 1.0.0
+     * @return bool
+     */
+    public function recieve() {
+        $data = new stdClass;
+        $data->module_id = false;
+        $data->track_id = false;
+        $data->external_id = false;
 
-		$data = file_get_contents('php://input');
+        $data = file_get_contents('php://input');
 
-		if( !$this->is_json( $data ) )
-			return false;
-		$data = json_decode( $data );
-		if( filter_input( INPUT_SERVER, 'REQUEST_METHOD' ) != 'POST' && $data->module_id ) {
-			return false;
-		}
+        if( !$this->is_json( $data ) )
+            return false;
+        $data = json_decode( $data );
+        if( filter_input( INPUT_SERVER, 'REQUEST_METHOD' ) != 'POST' && $data->module_id ) {
+            return false;
+        }
 
-		if( !is_object( $data ) )
-			return false;
+        if( !is_object( $data ) )
+            return false;
 
-		$id = (int) $data->external_id;
+        $id = (int) $data->external_id;
 
-		if( $data->track_id )
-			update_post_meta( $id, '_clarify_track_id', $data->track_id );
-		if( $data->bundle_id )
-			update_post_meta( $id, '_clarify_bundle_id',$data->bundle_id );
-	}
+        if( $data->track_id )
+            update_post_meta( $id, '_clarify_track_id', $data->track_id );
+        if( $data->bundle_id )
+            update_post_meta( $id, '_clarify_bundle_id',$data->bundle_id );
+    }
 
-	/**
-	 * Utility function to determine if a string is valid JSON
-	 *
-	 * @author Aaron Brazell <aaron@technosailor.com>
-	 * @since 1.0.0
-	 * @return bool
-	 *
-	 * @param $string
-	 *
-	 * @return bool
-	 */
-	public function is_json( $string ) {
-		$maybe = json_decode( $string );
-		if( is_array( $maybe ) || is_object( $maybe ) ) {
-			return true;
-		}
+    /**
+     * Utility function to determine if a string is valid JSON
+     *
+     * @author Aaron Brazell <aaron@technosailor.com>
+     * @since 1.0.0
+     * @return bool
+     *
+     * @param $string
+     *
+     * @return bool
+     */
+    public function is_json( $string ) {
+        $maybe = json_decode( $string );
+        if( is_array( $maybe ) || is_object( $maybe ) ) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
--- wp-clarify.php
+++ wp-clarify.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP-Clarify
 Plugin URI: http://github.com/Clarify/wp-clarify
-Verion: 1.0-RC1
+Version: 1.0-RC1
 Author: Aaron Brazell
 Author URI: http://technosailor.com
 Maintainer: Clarify, Inc
@@ -80,148 +80,148 @@ register_deactivation_hook( __FILE__, array( 'Clarify', 'deactivate' ) );
  */
 class Clarify {
 
-	/**
-	 * List of media types supported by Clarify
-	 * @var array
-	 *
-	 * @author Aaron Brazell <aaron@technosailor.com>
-	 * @since 1.0.0
-	 */
-	public $supported_media;
+    /**
+     * List of media types supported by Clarify
+     * @var array
+     *
+     * @author Aaron Brazell <aaron@technosailor.com>
+     * @since 1.0.0
+     */
+    public $supported_media;
 
-	/**
-	 * Constructor
-	 *
-	 * @author Aaron Brazell <aaron@technosailor.com>
-	 * @since 1.0.0
-	 */
-	public function __construct() {
+    /**
+     * Constructor
+     *
+     * @author Aaron Brazell <aaron@technosailor.com>
+     * @since 1.0.0
+     */
+    public function __construct() {
 
-		$this->supported_media = array(
-			'mpeg',
-			'mp3',
-			'wav',
-			'mp4',
-			'mov',
-			'ogg',
-			'flac',
-			'webm'
-		);
-		$this->hooks();
-	}
+        $this->supported_media = array(
+            'mpeg',
+            'mp3',
+            'wav',
+            'mp4',
+            'mov',
+            'ogg',
+            'flac',
+            'webm'
+        );
+        $this->hooks();
+    }
 
-	/**
-	 * Activation method
-	 *
-	 * @author Aaron Brazell <aaron@technosailor.com>
-	 * @since 1.0.0
-	 */
-	public static function activate() {}
+    /**
+     * Activation method
+     *
+     * @author Aaron Brazell <aaron@technosailor.com>
+     * @since 1.0.0
+     */
+    public static function activate() {}
 
-	/**
-	 * Deactivation method
-	 *
-	 * @author Aaron Brazell <aaron@technosailor.com>
-	 * @since 1.0.0
-	 */
-	public static function deactivate() {}
+    /**
+     * Deactivation method
+     *
+     * @author Aaron Brazell <aaron@technosailor.com>
+     * @since 1.0.0
+     */
+    public static function deactivate() {}
 
-	/**
-	 * WordPress hook interactions
-	 *
-	 * @author Aaron Brazell <aaron@technosailor.com>
-	 * @since 1.0.0
-	 */
-	public function hooks() {
+    /**
+     * WordPress hook interactions
+     *
+     * @author Aaron Brazell <aaron@technosailor.com>
+     * @since 1.0.0
+     */
+    public function hooks() {
 
-		add_action( 'plugins_loaded', function(){
-			$webhook = new Clarify_Webhooks_Bundle_Notify;
-			$webhook->recieve();
-		} );
+        add_action( 'plugins_loaded', function(){
+            $webhook = new Clarify_Webhooks_Bundle_Notify;
+            $webhook->recieve();
+        } );
 
-		add_action( 'transition_post_status', array( $this, 'save_post' ), 10, 3 );
-		add_action( 'init', array( $this, 'register_search' ) );
-		if( is_admin() ) {
-			add_action( 'init', array( $this, 'admin' ) );
-		}
+        add_action( 'transition_post_status', array( $this, 'save_post' ), 10, 3 );
+        add_action( 'init', array( $this, 'register_search' ) );
+        if( is_admin() ) {
+            add_action( 'init', array( $this, 'admin' ) );
+        }
 
-		add_filter( 'wp_audio_extensions', function( $types ){
-			$types[] = 'mpeg';
-			$types[] = 'flac';
-			return $types;
-		} );
+        add_filter( 'wp_audio_extensions', function( $types ){
+            $types[] = 'mpeg';
+            $types[] = 'flac';
+            return $types;
+        } );
 
-		add_filter( 'wp_video_extensions', function( $types ) {
-			$types[] = 'mov';
-			return $types;
-		} );
+        add_filter( 'wp_video_extensions', function( $types ) {
+            $types[] = 'mov';
+            return $types;
+        } );
 
-		add_action( 'plugins_loaded', array( $this, 'do_media' ),1 );
+        add_action( 'plugins_loaded', array( $this, 'do_media' ),1 );
 
-		add_action( 'wp_enqueue_scripts', function() {
-			wp_enqueue_script( 'jquery' );
-		});
-	}
+        add_action( 'wp_enqueue_scripts', function() {
+            wp_enqueue_script( 'jquery' );
+        });
+    }
 
-	/**
-	 * Instantiates the Player class
-	 *
-	 * @uses Clarify_Players
-	 * @author Aaron Brazell <aaron@technosailor.com>
-	 * @since 1.0.0
-	 */
-	public function do_media() {
-		$players = new Clarify_Players;
-	}
+    /**
+     * Instantiates the Player class
+     *
+     * @uses Clarify_Players
+     * @author Aaron Brazell <aaron@technosailor.com>
+     * @since 1.0.0
+     */
+    public function do_media() {
+        $players = new Clarify_Players;
+    }
 
-	/**
-	 * Instantiates the Search class
-	 *
-	 * @uses Clarify_Search
-	 * @author Aaron Brazell <aaron@technosailor.com>
-	 * @since 1.0.0
-	 */
-	public function register_search() {
-		new Clarify_Search;
-	}
+    /**
+     * Instantiates the Search class
+     *
+     * @uses Clarify_Search
+     * @author Aaron Brazell <aaron@technosailor.com>
+     * @since 1.0.0
+     */
+    public function register_search() {
+        new Clarify_Search;
+    }
 
-	/**
-	 * Instantiates the Admin class
-	 *
-	 * @uses Clarify_Admin
-	 * @author Aaron Brazell <aaron@technosailor.com>
-	 * @since 1.0.0
-	 */
-	public function admin() {
-		new Clarify_Admin;
-	}
+    /**
+     * Instantiates the Admin class
+     *
+     * @uses Clarify_Admin
+     * @author Aaron Brazell <aaron@technosailor.com>
+     * @since 1.0.0
+     */
+    public function admin() {
+        new Clarify_Admin;
+    }
 
-	/**
-	 * Fires notification to the Clarify API when a post is published
-	 *
-	 * @param $new_status
-	 * @param $old_status
-	 * @param $post
-	 *
-	 * @uses Clarify_Bundle_API
-	 * @author Aaron Brazell <aaron@technosailor.com>
-	 * @since 1.0.0
-	 *
-	 * @return bool
-	 */
-	public function save_post( $new_status, $old_status, $post ) {
-		if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
-			return false;
+    /**
+     * Fires notification to the Clarify API when a post is published
+     *
+     * @param $new_status
+     * @param $old_status
+     * @param $post
+     *
+     * @uses Clarify_Bundle_API
+     * @author Aaron Brazell <aaron@technosailor.com>
+     * @since 1.0.0
+     *
+     * @return bool
+     */
+    public function save_post( $new_status, $old_status, $post ) {
+        if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+            return false;
 
-		if ( wp_is_post_revision( $post->ID ) )
-			return false;
+        if ( wp_is_post_revision( $post->ID ) )
+            return false;
 
-		if( 'publish' != $new_status )
-			return false;
+        if( 'publish' != $new_status )
+            return false;
 
-		$api = new Clarify_Bundle_API;
-		return $api->save_bundle( $post->ID, $post );
-	}
+        $api = new Clarify_Bundle_API;
+        return $api->save_bundle( $post->ID, $post );
+    }
 }
 
 $clarifyio = new Clarify;
